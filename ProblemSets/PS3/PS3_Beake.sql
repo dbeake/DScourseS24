ca7a2acf5e26eaf7e264ae673c695607a0d50b29
Econ Account <ouecon002@schooner2.oscer.ou.edu>
2024-02-14 14:16:02 -0600

    finish PS3

---
+++ ProblemSets/PS3/PS3_Beake.sql
@@ -0,0 +1,351 @@
+CREATE TABLE PS3table(
+"var1" CHAR,
+"var2" INTEGER,
+"varN" REAL
+);
+.mode csv
+.import FL_insurance_sample.csv PS3table
+SELECT * FROM PS3table LIMIT 11;
+SELECT * FROM PS3table LIMIT 10;
+SELECT county FROM PS3table 
+;
+SELECT COUNTY FROM PS3table;
+SELECT County FROM PS3table;
+SELECT var3 FROM PS3table;
+SELECT "varN" FROM PS3table;
+--list unique values of the county variable) by creating a one-way frequency Table
+SELECT varN, COUNT(*) FROM PS3table GROUP BY varN;
+--Read in the Florida Insurance data CSV file
+--Create Table
+CREATE TABLE PS3table_2(
+"var1" CHAR,
+"var2" INTEGER,
+"VarN" REAL
+);
+--fill in table with data
+.mode csv
+.import FL_insurance_sample.csv PS3table_2;
+.import FL_insurance-sample.csv PS3table_2
+.import FL_insurance_sample.csv PS3table_2
+--Print out first 10 Rows of the Data SeT
+SELECT * FROM PS3table_2 LIMIT 10;
+--List which counties are in the sample: Create one-way frequency table
+SELECT VarN, COUNT(*) FROM PS3table_2 GROUP BY varN;
+--COMPUTE the avg. Property appreciation from 2011-2012
+--First I need to read in more variables from the CSV file
+CREATE TABLE
+;
+CREATE TABLE PS3table_n
+"var1" CHAR,
+"Var2" INTEGER,
+"Var3" REAL,
+"Var4" REAL,
+);
+"varN" REAL
+);
+--Average property appreciation from 2011 - 2012
+"SELECT AVG(tiv) AS
+avg_property-value
+FROM PS3table_2
+WHERE year >= 2011 AND year <= 2012 
+")
+;
+")
+;
+"SELECT AVG(tiv) AS
+avg_property_value
+FROM PS3table_2
+WHERE year >= 2011 AND year <= 2012
+)
+;
+SELECT AVG(tiv) AS
+avg_property_value
+FROM PS3table_2
+WHERE year >= 2011 and year <= 2012)
+;
+WHERE year >= 2011 and year <= 2012
+;
+("SELECT AVG(tiv) AS
+avg_property_value
+FROM PS3table_2
+WHERE year BETWEEN 2011 and 2012
+")
+;
+SELECT AVG(TIV) AS
+avg_property_value
+FROM PS3table_2
+WHERE year Between 2011 and 2012;
+SELECT AVG(tiv) AS
+avg_Property_Value
+FROM PS3table_2
+WHERE year BETWEEN 2011 and 2012;
+CREATE TABLE PS3TABLE_2(
+var1 TEXT,
+Var2 INTEGER,
+varN REAL
+);
+CREATE TABLE PS3TABE_3
+var1 TEXT,
+var2 INTEGER
+varN REAL
+var2 INTEGER,
+0;
+CREATE TABLE PS3TABLE_3
+var1 TEXT,
+var2 INTEGER,
+varN REAL
+);
+CREATE TABLE PSTABLE_3(
+"var1" TEXT,
+"var2" INTEGER,
+"varN" REAL
+);
+.mode csv
+.import FL_insurance_sample.csv PS3TABLE_3
+;
+--Print out first 10 Rows of Data Set
+SELECT * FROM PS3TABLE_3 LIMIT N;
+--Print out first 10 Rows of Data Set
+SELECT * FROM PS3TABLE_3 LIMIT 10;
+--List which counties are in the sample: Unique Values
+SELECT county, count(*) FROM PS3TABLE_3 GROUP BY county;
+--Compute the Average Property Appreciaton from 2011 t0 2012
+SELEC AVG(tiv) AS
+property_appreciation_value
+FROM PS3TABLE_3
+WHERE year BETWEEN 2011 and 2012;
+SELECT AVG(tiv) AS
+Property_appreciation_value
+FROM PS3TABLE_3
+WHERE year BETWEEN 2011 and 2012;
+.quit
+CREATE TABLE PS3TABLE-FL(
+"var1" TEXT
+;
+--Read File into SQL
+CREATE TABLE PS3table_FL(
+"var1" TEXT,
+"var2" INTEGER,
+"varN" REAL
+);
+.mode csv
+.import FL_insurance_sample.csv PS3table_FL
+--Print out first 10 rows of data set
+SELECT * FROM PS3table_FL LIMIT 10;
+mode csv
+;
+.mode csvb
+.mode csv
+.import FL_insurance_sample.csv PS3table_FL
+CREATE TABLE PS3table_FL1(
+var1 TEXT,
+var2 INtEGER,
+varN REAL
+);
+.mode csv
+.import FL_insurance_sample.csv PS3table_FL1
+Read File into SQL Script
+;
+--Read File into SQL Script
+CREATE TABLE PS3_FL
+var1 CHAR,
+var2 INTEGER,
+varN REAL
+);
+--Read File into SQL Script
+CREATE TABLE PS3_FL
+'var1' TEXT,
+'var2' INTEGER,
+'varN' REAL
+);
+--Read a File into SQL Script
+CREATE TABLE PS3_FL1(
+"var1" CHAR
+;
+--Read a file into SQL Script
+CREATE TABLE FL_TABLE(
+"var1" CHAR,
+"var2" INTEGER,
+"varN" REAL
+);
+.mode csv
+.import FL_insurance_sample.csv FL_TABLE;
+.import FL_insurance_sample.csv FL-TABLE
+.import FL_insurane_sample.csv FL_TABLE
+.import FL_insurance_sample.csv FL_TABLE
+./PS3.sh
+SELECT * FROM FL_insurnace_sample.csv LIMIT 5;
+.quit
+.read FL_insurance_sample.csv
+--Read a file into SQL Script
+CREATE TABLE PS3-table (
+"var1" TEXT,
+"var2" TExt,
+"var3" TEXT,
+"var4" TEXT,
+"var5" TEXT,
+"var6" TEXT,
+"var7" TEXT,
+"var8" TEXT,
+"var9" TEXT,
+"var10 TeXT,
+"var11" TEXT
+);
+;
+):
+);
+;
+"varN" Real
+);
+;
+.open PS3TABLE_FL
+;
+;
+.quit
+.quit
+.tables
+;
+schema PS3table_FL
+;
+.schema PS3table_FL
+;
+.schema PS3Beake_table
+;
+.tables
+;
+.tables;
+.help
+.import 
+.import FL_insurance_sample.csv
+;
+.restore
+.restore file
+;
+.quit
+--Read a file into SQL script
+CREATE TABLE PS3FL_table(
+"policyID" TEXT,
+;
+.quit
+.schema PS3table
+;
+.quit
+--Read a file into SQL script
+CREATE TABLE PS3FL_table(
+"policyID" INTEGER,
+"statecode" CHAR,
+"county" TEXT,
+"eq_site_limit" TEXT,
+"hu_site_limit" REAL,
+"fl_site_limit" REAL,
+"fr_site_limit" REAL,
+"tiv_2011" TEXT,
+);
+.quit
+--Read a file into SQL script
+CREATE TABLE PS3table (
+"var1" Text,
+"var2" TExt,
+"varN" REAL
+);
+;
+;
+.mode csv
+.import FL_insurance_sample.csv PS3table
+SELECT * FROM PS3table LIMIT 10;
+--List which Counties are in the Sample: Unique Values
+SELECT county, COUNT(*) FROM PS3table GROUP BY county;
+SELECT "county" COUNT(*) FROM PS3table GROUP BY "county";
+SELECT "count", COUNT(*) FROM PS3table GROUP BY "county";
+SELECT "COUNTY", COUNT(*) FROM PS3table GROUP BY "county";
+SELECT "varN", COUNT(*) FROM PS3table GROUP BY "varN"'
+;
+;
+.quit
+akl;djf;likasjf
+;
+;
+);
+ljkfhlakjsdfhsldkfhf;klj;iasjd
+SELECT "varN", COUNT(*) FROM PS3table GROUP BY "varN";
+;
+.quit
+;
+quit
+--Read a File into SQL Script
+CREATE TABLE PS3table (
+"policyID
+"policyID
+;
+CREATE TABLE PS3table (
+"policyID" INTEGER,
+"statecode" CHAR,
+"county" TEXT,
+"eq_site_limit" REAL,
+"hu_site_limit" REAL,
+"fl_site_limit" REAL,
+"fr_site_limit" REAL,
+"tiv_2011" REAL,
+"tiv_2012" REAL,
+"eq_site_deductible" REAL,
+"hu_site_deductible" REAL,
+"fl_site_deductible" REAL,
+"fr_site_deductible" REAL,
+"point_latitude" REAL,
+"point_longitude" REAL,
+"line" REAL,
+"construction" TEXT,
+"point_granularity" INTEGER
+);
+--Fill in table with Data
+.mode csv
+.import FL_insurance_sample.csv PS3table
+;
+--Printout Fist 10 Rows of the Dataset
+SELECT * FROM PS3table LIMIT 10;
+--List Which Counties are in the sample
+SELECT county, COUNT(*) FROM PS3table GROUP BY county;
+--Compute the Average Property Appreciation form 2011 to 2012
+SELECT AVG(tiv_2012 - tiv_2011) AS
+average_appreciation
+FROM PS3table;
+--Create a Frequency table of the construction variable: what material are buildings made of 
+SELECT construction, COUNT(*) AS
+frequency,
+ROUND(CAST(COUNT(*) AS FLOAT) / (SELECT COUNT(*) FROM PS3table), 2) AS
+fraction
+FROM PS3table
+GROUP BY constuction;
+GROUP BY construction;
+SELECT construction, COUNT(*) AS frequency, (COUNT(*) * 100) / (SELECT COUNT(*) FROM PS3table AS fraction FROM PS3table GROUP BY construction;
+SELECT construction, COUNT(*) AS
+frequency,
+(COUNT(*) * 100) / (SELECT COUNT(*) 
+FROM PS3table AS Fraction
+FROM Ps3table GROUP BY construction;
+SELECT construction, COUNT(*) AS frequency,
+ROUND((COUNT(*) * 1.0 / (SELECT COUNT(*) FROM PS3table)) * 100m 2) AS fraction
+FROM PS3table GROUP BY construction;
+ROUND((COUNT(*) * 1.0 / (SELECT COUNT(*) FROM PS3table)) * 100, 2) AS fraction
+FROM PS3table GROUP BY construction;
+SELECT construction, COUNT(*) AS Frequency,
+(COUNT(*) * 100) / (SELECT COUNT(*) FROM PS3table AS fraction
+FROM PS3table
+GROUP BY construction;
+SELECT construction, COUNT(*) AS 
+frequency,
+(COUNT(*) * 100) / (SELECT COUNT(*)
+FROM PS3table AS fraction
+FROM PS3table
+GROUP BY construction;
+SELECT construction, COUNT(*) AS frequency,
+ROUND(COUNT(*) * 100 / (SELECT COUNT(*) FROM PS3table), 2) AS
+fraction
+FROM PS3table GROUP BY construction;
+.output PS3table.sqlite3
+.dump
+;
+.output PS3table.sqlite3;
+.dump
+;
+.output PS3script.sqlite3
